7b8177b020515a4f22f0e487676b5d352c8ce92b
Wagner Santos <35262291-wagnsantos02@users.noreply.replit.com>
2024-09-11 23:53:42 +0000

    feat: Refatorando as rotas

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -1,4 +1,4 @@
-import pool from '../database';
+import pool from "../config/database";
 
 const createUsersTable = async () => {
   const client = await pool.connect();
@@ -13,7 +13,7 @@ const createUsersTable = async () => {
     await client.query(queryText);
     console.log('Tabela "users" criada com sucesso!');
   } catch (err) {
-    console.error('Erro ao criar tabela:', err);
+    console.error("Erro ao criar tabela:", err);
   } finally {
     client.release();
   }
